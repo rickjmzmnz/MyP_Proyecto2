43e3a83a769fe727324afce378f37d1a8257b926
Ricardo Jimenez Mendez <rickjmzmnz@ciencias.unam.mx>
2015-10-08 20:14:04 -0500

    first commit

---
+++ src/proyecto2/FXMLDocumentController.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto2;
+
+import java.sql.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author ricardo
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO 
+    /**  Connection c = null;
+    try {
+      Class.forName("org.sqlite.JDBC");
+      c = DriverManager.getConnection("jdbc:sqlite:./lib/ricardo.db");
+      String cadena = "INSERT INTO tabla (id_t) VALUES (1)";
+      Statement est = c.createStatement();
+      est.executeUpdate(cadena);
+      est.close();
+      //c.commit();
+      c.close();
+    } catch ( Exception e ) {
+      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+      System.exit(0);
+    }
+    System.out.println("Opened database successfully");
+  }*/
+    }    
+}
+
