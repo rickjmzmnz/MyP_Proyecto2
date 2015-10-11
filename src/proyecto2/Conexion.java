eebc2fa7dc96cb21bf5d0873e4c021f802fa4e1f
Ricardo Jimenez Mendez <rickjmzmnz@ciencias.unam.mx>
2015-10-11 12:39:46 -0500

    second commit

--- src/proyecto2/Conexion.java
+++ src/proyecto2/Conexion.java
@@ -5,10 +5,38 @@
  */
 package proyecto2;
 
+import java.sql.*;
+
 /**
  *
  * @author ricardo
  */
 public class Conexion {
+
+    public static Connection conexion;
+
+    public synchronized static Connection abrir() {
+        try {
+            if (conexion == null || conexion.isClosed()) {
+                Class.forName("org.sqlite.JDBC");
+                conexion = DriverManager.getConnection("jdbc:sqlite:./lib/rickys_toys.db");
+            }
+        } catch (Exception e) {
+            //mandar errores
+        }
+        return conexion;
+    }
+
+    public synchronized static void cerrar() {
+        try {
+            if(conexion == null || conexion.isClosed()) {
+                return;
+            } else {
+                conexion.close();
+            }
+        } catch (Exception e) {
+            //agregar
+        }
+    }
     
 }
--- src/proyecto2/Marca.java
+++ src/proyecto2/Marca.java
@@ -5,10 +5,32 @@
  */
 package proyecto2;
 
+import java.sql.*;
+
 /**
  *
  * @author ricardo
  */
 public class Marca {
+
+    private int idMarca;
+    private String nombreMarca;
     
+    public Marca(int idMarca,String nombreMarca) {
+        idMarca = this.idMarca;
+        nombreMarca = this.nombreMarca;
+    }
+    
+    public synchronized void agregar() throws SQLException {
+        Connection conexion = Conexion.abrir();
+        Statement declaracion = conexion.createStatement();
+        String cadena = "";
+        cadena = cadena.concat("INSERT INTO marca (id_marca,nombre_marca) "
+                + "VALUES (" + this.idMarca + "," + this.nombreMarca + ");");
+        declaracion.executeUpdate(cadena);
+        declaracion.close();
+        //conexion.commit();
+        Conexion.cerrar();
+    }
+            
 }
